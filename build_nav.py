15e516b4196a3495b508140f45b7ea12f4dfa802
Ethan <ethanhcurtiss@gmail.com>
2021-06-24 00:29:59 -0400

    add file path

--- build_nav.py
+++ build_nav.py
@@ -1,5 +1,8 @@
 import os
 
+root_path = "docs"
+path_substring_start = len(root_path) + 1
+
 def recurse_tree(path, depth):
     nav_string = ""
 
@@ -13,11 +16,12 @@ def recurse_tree(path, depth):
             file_name_tuple = os.path.splitext(dirEntry.path)
             if file_name_tuple[1] == ".md":
                 file_name = dirEntry.name.split(".")[0]
-                nav_string += "  " * depth + "- " + file_name + ": " + dirEntry.name + "\n"
+                relative_file_path = dirEntry.path[path_substring_start:]
+                nav_string += "  " * depth + "- " + file_name + ": " + relative_file_path + "\n"
     
     return nav_string
 
-nav = "\n\nnav:\n" + recurse_tree("./docs", 1)
+nav = "\n\nnav:\n" + recurse_tree(root_path, 1)
 
 with open("mkdocs.yml", "a") as f:
     f.write(nav)
